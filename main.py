215f32ae53a38fea10c6bc51572b921e9bfa251d
Stefan Stumpf <stefan.stumpf92@googlemail.com>
2023-12-20 17:02:54 +0100

    20231220_1702

---
+++ main.py
@@ -0,0 +1,26 @@
+import pygame as pg
+import constants as c
+
+
+
+pg.init()
+
+clock = pg.time.Clock()
+
+screen = pg.display.set_mode((c.SCREEN_WIDTH,c.SCREEN_HEIGHT))
+pg.display.set_caption('Tower Defense')
+
+
+#load images
+enemy_image = pg.image.load()
+
+
+run = True
+while run:
+    clock.tick(c.FPS)
+    
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            run = False
+
+pg.quit()
