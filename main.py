e850a0c261d9c4d4d95384a4a2460d72fa993735
Stefan Stumpf <stefan.stumpf92@googlemail.com>
2023-12-20 17:23:18 +0100

    2

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame as pg
 import constants as c
+from enemy import Enemy
 
 
 
@@ -12,15 +13,32 @@ pg.display.set_caption('Tower Defense')
 
 
 #load images
-enemy_image = pg.image.load()
+enemy_image = pg.image.load('assets/images/enemies/enemy_1.png').convert_alpha()
+
+#creat group
+enemy_group = pg.sprite.Group()
+enemy_1 = Enemy((200,300),enemy_image)
+enemy_group.add(enemy_1)
 
 
 run = True
 while run:
     clock.tick(c.FPS)
+    screen.fill('grey100')
+
+
+    #update groups
+    enemy_group.update()
+
+    #draw groups
+    enemy_group.draw(screen)
     
     for event in pg.event.get():
         if event.type == pg.QUIT:
             run = False
+            
+
+
+    pg.display.flip()
 
 pg.quit()
